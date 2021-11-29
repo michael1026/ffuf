4512d199eb52db9a100af5b1636159c548add682
Michael Blake <michael1026@live.com>
2021-11-28 23:42:48 -0800

    update version

--- pkg/ffuf/version.go
+++ pkg/ffuf/version.go
@@ -2,7 +2,7 @@ package ffuf
 
 var (
 	//VERSION holds the current version number
-	VERSION = "1.3.2"
+	VERSION = "1.3.4"
 	//VERSION_APPENDIX holds additional version definition
 	VERSION_APPENDIX = "-dev"
 )
